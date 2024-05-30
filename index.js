ad4ec58ff884cdcccf86b3629a01df6a8c547c3f
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-05-30 18:54:38 +0100

    Session 4 codepen working

---
+++ index.js
@@ -0,0 +1,38 @@
+const userInput = document.getElementById('userInput');
+const addButton = document.getElementById('addButton');
+const removeButton = document.getElementById('removeButton');
+const division = document.getElementById('division');
+
+
+addButton.addEventListener('click', function () {
+  const text = userInput.value;
+
+  // Check if the input is not empty
+  if (text) {
+    const divItem = document.createElement('p');
+    divItem.textContent = text;
+
+    divItem.addEventListener('mouseover', function () {
+      divItem.style.backgroundColor = 'lightgrey';
+      divItem.style.fontWeight = 'bold';
+      divItem.style.transition = '.5s'
+
+    });
+
+    divItem.addEventListener('mouseout', function () {
+      divItem.style.backgroundColor = '';
+      divItem.style.fontWeight = '';
+    });
+
+    division.appendChild(divItem);
+    userInput.value = '';
+  }
+});
+
+removeButton.addEventListener('click', function () {
+  const divItems = division.getElementsByTagName('p');
+
+  if (divItems.length > 0) {
+    division.removeChild(divItems[divItems.length - 1]);
+  }
+});
