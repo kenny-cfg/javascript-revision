01437addb1f5eee0de41b956f5f07aaef7956e5d
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-05-30 19:18:39 +0100

    Extract paragraph creation code into its own function

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ const addButton = document.getElementById('addButton');
 const removeButton = document.getElementById('removeButton');
 const division = document.getElementById('division');
 
+function createParagraphElement(text) {
+  const paragraphElement = document.createElement('p');
+  paragraphElement.textContent = text;
+
+  paragraphElement.addEventListener('mouseover', function () {
+    paragraphElement.style.backgroundColor = 'lightgrey';
+    paragraphElement.style.fontWeight = 'bold';
+    paragraphElement.style.transition = '.5s'
+
+  });
+
+  paragraphElement.addEventListener('mouseout', function () {
+    paragraphElement.style.backgroundColor = '';
+    paragraphElement.style.fontWeight = '';
+  });
+
+  return paragraphElement
+}
+
 function onClickAddButtonHandler() {
   const text = userInput.value;
 
-  // Check if the input is not empty
   if (text) {
-    const divItem = document.createElement('p');
-    divItem.textContent = text;
-
-    divItem.addEventListener('mouseover', function () {
-      divItem.style.backgroundColor = 'lightgrey';
-      divItem.style.fontWeight = 'bold';
-      divItem.style.transition = '.5s'
-
-    });
-
-    divItem.addEventListener('mouseout', function () {
-      divItem.style.backgroundColor = '';
-      divItem.style.fontWeight = '';
-    });
-
-    division.appendChild(divItem);
+    const paragraphElement = createParagraphElement(text);
+    division.appendChild(paragraphElement);
     userInput.value = '';
   }
 }
